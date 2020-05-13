ba63821a08474bb10ad0b64add77daed882abe51
yuwei21 <55149858+yuwei21@users.noreply.github.com>
2020-05-13 00:56:40 -0500

    Add files via upload

---
+++ src/download_data.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def download_data (read_path, save_path):
+    """Download data from a public S3 bucket to local folder
+    Args:
+        read_path (str) : path of data source
+        save_path (str) ; path to save the data after download
+    Returns : None 
+    """
+
+    try:
+        df = pd.read_csv(read_path, sep=",")
+        df.to_csv(save_path)
+        logger.info("Download file from %s to %s", read_path, save_path)
+    except Exception as e:
+        logger.error(e)
+        
+if __name__ == "__main__":
+    url = "https://s3-public-nw.s3.us-east-2.amazonaws.com/insurance-source/insurance.csv"
+    download_data(url, "./data/insurace.csv")
+    
---
+++ src/sql/db_create.py
@@ -0,0 +1,83 @@
+import sqlalchemy as sql
+import pandas as pd
+import os
+import sys
+import logging
+import logging.config
+from sqlalchemy import Column, Integer, String, MetaData, create_engine, Text, Float
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import config
+from helpers import create_connection, get_session
+import argparse
+
+
+logging.basicConfig(level=logging.DEBUG, filename="logfile", filemode="a+",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+
+logger = logging.getLogger('insurance-db')
+Base = declarative_base()
+
+class Insurance_Predict(Base):
+    """ Defines the data model for the table `insurance`. """
+
+    __tablename__ = 'insurance_predict'
+
+    patient_id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    age = Column(Integer, unique=False, nullable=False)
+    sex = Column(String(100), unique=False, nullable=False)
+    bmi = Column(Float, unique=False, nullable=False)
+    smoker = Column(String(100), unique=False, nullable=False)
+    region  = Column(String(100), unique=False, nullable=False)
+    charges = Column(Float, unique=False, nullable=False)
+
+    predicted_charge = Column(Float, unique=False, nullable=False)
+
+
+    def __repr__(self):
+        insurance_repr = "<Insurance_Predict(patient_id='%i', age='%i', sex='%s', bmi='%f', smoker='%s', region='%s', charges='%f', predicted_charge='%f' )>"
+        return insurance_repr % (self.patient_id, self.age, self.sex, self.bmi, self.smoker, self.region, self.charges,self.predicted_charge)
+
+
+
+def get_engine_string(RDS = False):
+    """ Get database engine path. """
+    if RDS:
+        conn_type = "mysql+pymysql"
+        user = os.environ.get("MYSQL_USER")
+        password = os.environ.get("MYSQL_PASSWORD")
+        host = os.environ.get("MYSQL_HOST")
+        port = os.environ.get("MYSQL_PORT")
+        DATABASE_NAME = 'msia423_db'
+        engine_string = "{}://{}:{}@{}:{}/{}". \
+            format(conn_type, user, password, host, port, DATABASE_NAME)
+        # print(engine_string)
+        logging.debug("engine string:{}".format(engine_string))
+        return  engine_string
+    else:
+        return 'sqlite:///insurance.db'
+
+
+
+def create_db(args,engine=None):
+    """Creates a database with the data models inherited from `Base` (Insurance).
+    Args:
+        engine (:py:class:`sqlalchemy.engine.Engine`, default None): SQLAlchemy connection engine.
+    Returns:
+        None
+    """
+    if engine is None:
+        RDS = eval(args.RDS)
+        logger.info("RDS:%s",RDS)
+        engine = sql.create_engine(get_engine_string(RDS = RDS))
+
+    Base.metadata.create_all(engine)
+    logging.info("database created")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create defined tables in database")
+    parser.add_argument("--RDS", default="False",help="True if want to create in RDS else None")
+    args = parser.parse_args()
+    create_db(args)
+
